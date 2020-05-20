dcf5813594071a3e75980bea37d3a714a65b1ddd
inaba <software@panduro.guru>
2020-05-20 20:15:43 +0200

    Post bottom

--- client/src/components/compose.vue
+++ client/src/components/compose.vue
@@ -19,9 +19,9 @@
           placeholder="Play sound of bird"
         />
       </section>
-      <section>
-        <div></div>
-        <div>
+      <section class="flex justify-between">
+        <div class="self-center">Icons</div>
+        <div class="self-center">
           <button
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
