21e8e356ab46efbb7b6e5c833854422d63230f36
inaba <software@panduro.guru>
2020-05-20 16:24:18 +0200

    Removed defaults and added routing

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <section id="app">
+    <Navigation />
+
+    <main>
+      <router-view />
+    </main>
+  </section>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import Navigation from "@/components/navigation.vue";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@Component({
+  components: {
+    Navigation
   }
-}
-</style>
+})
+export default class Main extends Vue {}
+</script>
