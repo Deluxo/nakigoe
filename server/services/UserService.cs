18c881bd2b22096031977beba34bac9a8aecd5e9
inaba <software@panduro.guru>
2020-05-24 01:23:32 +0200

    Added a base user service

---
+++ server/services/UserService.cs
@@ -0,0 +1,49 @@
+using System.Linq;
+using Server.Data;
+using BCrypt.Net;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System;
+using System.Text;
+
+namespace Server.Services
+{
+  public interface IUSerService
+  {
+    string Authenticate(string username, string password);
+  }
+
+  public class UserService : IUSerService
+  {
+
+    readonly UserContext _context;
+    public UserService(UserContext context)
+    {
+      _context = context;
+    }
+
+    public string Authenticate(string username, string password)
+    {
+      var user = _context.Users.First(user => user.UserName == username);
+
+      var comparePasswords = BCrypt.Net.BCrypt.Verify(user.Password, password);
+
+      if (!comparePasswords)
+        return null;
+
+      var key = Encoding.ASCII.GetBytes("//TODO: Change me");
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new System.Security.Claims.ClaimsIdentity(new Claim[] {
+          new Claim(ClaimTypes.Name, user.Id.ToString())
+        }),
+        Expires = DateTime.UtcNow.AddDays(1),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+      };
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      return tokenHandler.WriteToken(token);
+    }
+  }
+}
