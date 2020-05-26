fb0289eedca1e1e6e6ad63c2bc0bbe0931a86f79
inaba <software@panduro.guru>
2020-05-26 12:54:04 +0200

    Set up a register endpoint

---
+++ server/Controllers/RegisterController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Server.Services;
+using Server.Models.InputModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Server.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class RegisterController : ControllerBase
+  {
+    readonly IUSerService _userService;
+    public RegisterController(IUSerService userService)
+    {
+      _userService = userService;
+    }
+
+    [HttpPost]
+    public IActionResult post([FromBody] RegisterModel registerModel)
+    {
+      var userExists = _userService.IfUserExists(registerModel.UserName);
+      if (userExists)
+        return Unauthorized("Username has already been taken");
+
+      var token = _userService.Register(registerModel);
+
+      return Ok(token);
+    }
+  }
+}
