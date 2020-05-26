5c2fbc1e220449c208d06f1c3bc80799cd585371
inaba <software@panduro.guru>
2020-05-26 17:03:08 +0200

    Created user component

---
+++ client/src/components/Register.vue
@@ -0,0 +1,10 @@
+<template>
+  <form></form>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Register extens Vue { }
+</script>
