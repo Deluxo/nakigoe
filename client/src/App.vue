181574fcc95983971bc767fc3902575f5d3f6b01
inaba <software@panduro.guru>
2020-05-27 00:49:54 +0200

    Registering is supposed to be done through a modal but this will do for the beginning.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <section id="app" class="flex container mx-auto">
-    <Navigation class="w-1/5 bg-gray-300" />
+    <section v-if="loggedIn">
+      <Register />
+    </section>
 
-    <main class="w-4/5 bg-gray-100">
-      <router-view />
+    <main v-else class="flex w-full">
+      <Navigation class="w-1/5 bg-gray-300" />
+
+      <section class="w-4/5 bg-gray-100">
+        <router-view />
+      </section>
     </main>
   </section>
 </template>
@@ -12,11 +18,15 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import Navigation from "@/components/navigation.vue";
+import Register from "@/components/Register.vue";
 
 @Component({
   components: {
     Navigation,
+    Register,
   },
 })
-export default class Main extends Vue {}
+export default class Main extends Vue {
+  loggedIn = false;
+}
 </script>
