d5b912c20dbabb8b936df552224d6a57594ff1fe
inaba <software@panduro.guru>
2020-05-28 23:06:26 +0200

    Added empty Registration methods

--- client/src/store/auth.ts
+++ client/src/store/auth.ts
@@ -8,12 +8,27 @@ type User = Object;
 export default class Auth extends VuexModule {
   private Token: string | null = null;
   private User: User | null = null;
-  
+  private IsAuthenticated = false;
+
   get token(): string | null {
     return this.Token;
   }
 
   get user(): User | null {
     return this.User;
+  }
+
+  get isAuthenticated(): boolean {
+    return this.IsAuthenticated;
+  }
+
+  @Mutation
+  Authenticate() {
+
+  }
+
+  @Action
+  Register() {
+
   }
 }
