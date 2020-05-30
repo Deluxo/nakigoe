bceb2949b92e8d91cadd53b275970cbcf586ac91
inaba <software@panduro.guru>
2020-05-30 23:45:11 +0200

    Function to create empty model

--- client/src/models/auth/register-model.ts
+++ client/src/models/auth/register-model.ts
@@ -5,3 +5,12 @@ export type RegisterModel = {
   DisplayName: string;
   Bio: string;
 }
+
+export const createEmptyModel = 
+  (): RegisterModel => ({
+    UserName: "",
+    Password: "",
+    BirthDate: new Date(Date.now()),
+    DisplayName: "",
+    Bio: "",
+  });
