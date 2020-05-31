2d230f961165975007bd4c06031460caf43fe82e
inaba <software@panduro.guru>
2020-06-01 00:39:44 +0200

    Created the base for a text input

--- client/src/components/auth/register.vue
+++ client/src/components/auth/register.vue
@@ -1,11 +1,20 @@
 <template>
   <form>
     <h2 class="text-xl font-bold">Create your account</h2>
-    <section class="login-details">
-      <input type="text" name="user-name" placeholder="Your username" v-model="model.UserName">
+    <section class="input login-details">
+      <label for="user-name">Username</label>
+      <input 
+        type="text" 
+        name="user-name"
+        id="user-name" 
+        placeholder="Your username" 
+        v-model="model.UserName">
     </section>
 
-    <button class="button button-blue" @click="printModel">
+    <button 
+      class="button button-blue"
+      @click="printModel"
+      type="button">
       Check model
     </button>
   </form>
@@ -24,7 +33,10 @@ export default class Register extends Vue {
   }
 
   printModel() {
-    console.log(this.model);
+    if (!this.model)
+      return;
+
+    console.log(JSON.stringify(this.model));
   }
 }
 </script>
