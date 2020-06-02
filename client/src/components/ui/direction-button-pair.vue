ab1c4bd11f662bc3a876a364e14a88895c2cddda
inaba <software@panduro.guru>
2020-06-03 00:38:31 +0200

    Added a way to hide and show the directional buttons

---
+++ client/src/components/ui/direction-button-pair.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <DirectionButton 
+      v-if="!hideBackwards"
+      class="direction-button"
+      @click="switchForm"
+      direction="backwards"/>
+
+    <DirectionButton 
+      v-if="!hideForwards"
+      class="direction-button"
+      @click="switchForm"
+      direction="forwards"/>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import DirectionButton from "@/components/ui/direction-button.vue";
+
+@Component({
+  components: {
+    DirectionButton,
+  },
+})
+export default class DirectionButtonPair extends Vue {
+  @Prop({ default: false })
+  hideForwards!: boolean;
+
+  @Prop({ default: false })
+  hideBackwards!: boolean;
+
+  switchForm(direction: number) {
+    this.$emit("click", direction);
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/styles/components/buttons";
+
+.direction-button {
+  @apply button;
+  @apply button-blue;
+  @apply button-small;
+}
+</style>
