fd2fd0d513f2b5364b50da5b728be6baa6c6d0a3
inaba <software@panduro.guru>
2020-06-02 16:50:55 +0200

    This is about the components needed

--- client/src/components/auth/register.vue
+++ client/src/components/auth/register.vue
@@ -1,22 +1,49 @@
 <template>
   <form>
     <h2 class="text-xl font-bold">Create your account</h2>
-    <section class="input login-details">
-      <label for="user-name">Username</label>
-      <input 
-        type="text" 
-        name="user-name"
-        id="user-name" 
-        placeholder="Your username" 
-        v-model="model.UserName">
+    
+    <section class="user-details">
+      <h3 class="font-bold">First we need some login details</h3>
+
+      <InputComponent 
+        label="Username"
+        placeholder="Your login name"
+        v-model="model.UserName"
+        limit="50"/>
+
+      <InputComponent 
+        label="Password"
+        placeholder="Password"
+        v-model="model.Password"
+        type="password"/>
     </section>
 
-    <InputComponent 
-      label="Password"
-      v-model="model.Password"
-      type="text"
-      limit="50"/>
+    <section class="personal-details">
+      <h3 class="font-bold">Now for some information about you</h3>
 
+      <InputComponent 
+        label="Name"
+        placeholder="Display name"
+        v-model="model.Display"
+        limit="25"/>
+
+      <InputComponent 
+        label="Birthdate"
+        placeholder="Birthdate"
+        v-model="model.BirthDate"
+        type="date"/>
+    </section>
+
+    <section class="bio">
+      <h3>And finally a short description of yourself</h3>
+      <InputComponent 
+        label="Bio"
+        placeholder="Say something nice about yourself"
+        type="textarea"
+        v-model="model.Bio"
+        limit="140"/>
+    </section>
+   
     <button 
       class="button button-blue"
       @click="printModel"
