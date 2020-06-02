999b76542db48aaa1b1426c02086661e835552bf
inaba <software@panduro.guru>
2020-06-02 23:30:19 +0200

    Added a directional button component

---
+++ client/src/components/ui/DirectionButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <button 
+    class="button button-blue"
+    @click="emit"
+    type="button">{{ directions[direction].name }}</button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+interface Direction {
+  name: string;
+  number: number;
+}
+
+// Temp permanent hack to lookup directions
+interface Directions {
+  [key: string]: Direction;
+}
+
+const directions: Directions = { 
+    forwards: { 
+      name: "Next",
+      number: 1,
+    },
+    backwards: {
+      name: "Previous",
+      number: -1,
+    },
+  };
+
+@Component
+export default class Input extends Vue {
+  @Prop()
+  direction!: string;
+
+  private validDirections = ["forwards", "backwards"];
+  
+  created() {
+    const isInvalidDirecion = 
+      this.validDirections.findIndex(validDirecion => validDirecion === this.direction) === -1;
+
+    if (isInvalidDirecion) {
+      throw Error("The direction needs to be either forwards or backwards");
+    }  
+  }
+
+  emit() {
+      if (!this.direction) return; //
+
+    const direction = directions[this.direction].number;
+    this.$emit("click", direction);
+  }
+}
+</script>
