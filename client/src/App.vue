19828f0962f8c0398ac038289dc4236f892c22ce
inaba <software@panduro.guru>
2020-06-10 01:52:59 +0200

    Implemented the modal

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <section id="app" class="flex container mx-auto">
     <section v-if="!loggedIn">
-      <Register />
+      <button 
+        class="button"
+        @click="showRegister">Register</button>
+      
+      <Modal ref="register-modal">
+        <Register />
+      </Modal>
     </section>
 
     <main v-else class="flex w-full">
@@ -19,15 +25,25 @@ import Vue from "vue";
 import Component from "vue-class-component";
 import Navigation from "@/components/navigation.vue";
 import Register from "@/components/auth/register.vue";
+import Modal from "@/components/ui/modal.vue";
+import { Ref } from "vue-property-decorator";
 
 @Component({
   components: {
     Navigation,
     Register,
+    Modal,
   },
 })
 export default class Main extends Vue {
   loggedIn = false;
+
+  @Ref("register-modal")
+  private registerModal!: Modal;
+
+  showRegister() {
+    this.registerModal.toggleShown();
+  }
 }
 </script>
 
