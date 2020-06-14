f150d1579d50d2eeee23facd84f98e3083c740f2
inaba <software@panduro.guru>
2020-06-14 14:36:16 +0200

    Created input component

---
+++ client/src/components/auth/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Login extends Vue {
+
+}
+</script>
