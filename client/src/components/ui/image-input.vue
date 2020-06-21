66a6c9a27f473ce76a09655dfcfca1bfc14c2003
inaba <software@panduro.guru>
2020-06-21 23:38:48 +0200

    Created the base image input

---
+++ client/src/components/ui/image-input.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class ImageInput extends Vue {
+
+}
+</script>
