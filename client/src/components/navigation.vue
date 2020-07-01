1fc6aba97ec291e401d473e58a033218a2b1103f
inaba <software@panduro.guru>
2020-07-02 01:23:17 +0200

    Added the icons to the nav as well

--- client/src/components/navigation.vue
+++ client/src/components/navigation.vue
@@ -1,22 +1,47 @@
 <template>
   <aside>
     <nav>
-      <router-link class="block pt-2 pb-2" to="/">Home</router-link>
-      <router-link class="block pt-2 pb-2" to="/notifications"
-        >Notifications</router-link
-      >
-      <router-link class="block pt-2 pb-2" to="/messages">Messages</router-link>
-      <router-link class="block pt-2 pb-2" to="/profile">Profile</router-link>
+      <router-link class="nav-link" to="/">
+        <Icon 
+          icon="add"
+          text="Home"/>
+      </router-link>
+      <router-link class="nav-link" to="/notifications">
+        <Icon 
+          icon="bulb"
+          text="Notifications"/>
+      </router-link>
+      <router-link class="nav-link" to="/messages">
+        <Icon 
+          icon="message"
+          text="Messages"/>
+      </router-link>
+      <router-link class="nav-link" to="/profile">
+        <Icon 
+          icon="user"
+          text="Profile"/>
+      </router-link>
     </nav>
   </aside>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.nav-link {
+  @apply block;
+  @apply pt-2;
+  @apply pb-2;
+}
+</style>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import Icon from "@/components/ui/icon.vue";
 
-@Component
+@Component({
+  components: {
+    Icon,
+  },
+})
 export default class Navigation extends Vue {}
 </script>
