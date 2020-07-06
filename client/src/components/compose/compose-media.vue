d7ddffbf5beb55a35a89723e8c28140580ae1ecd
Lukas Levickas <l.levickas@protonmail.com>
2020-07-06 18:39:50 +0300

    refactor(media-component): simplify validation

--- client/src/components/compose/compose-media.vue
+++ client/src/components/compose/compose-media.vue
@@ -1,82 +1,44 @@
 <template>
   <section>
-    <input 
-      v-bind="media"
-      ref="image-input"
-      type="file" 
-      name="files" 
-      class="hidden"
-      @change="imageSelected"
-      multiple>
+    <input v-bind="media" ref="image-input" type="file" name="files" class="hidden" @change="onSelected" multiple />
+    <small v-if="errorMessage">{{ this.errorMessage }}</small>
   </section>
 </template>
 
 <script lang="ts">
-import {
- Component, Prop, Vue, Ref, 
-} from "vue-property-decorator";
+  import { Component, Vue, Ref } from "vue-property-decorator";
 
-@Component
-export default class ComposeMedia extends Vue {
-  private media!: File[];
-  private validMediaTypes = /(video|image)\//;
+  @Component
+  export default class ComposeMedia extends Vue {
+    private media!: File[];
+    private validMediaTypes = /(video|image)\//;
+    private errorMessage: string | null = null;
 
-  @Ref("image-input")
-  imageInput!: HTMLInputElement;
+    @Ref("image-input")
+    imageInput!: HTMLInputElement;
 
-  created() {
-    this.media = [];
+    created() {
+      this.media = [];
+    }
+
+    addImages() {
+      this.imageInput.click();
+    }
+
+    onSelected() {
+      const files = this.imageInput.files === null ? [] : Array.from(this.imageInput.files);
+      const videoFile = files.find((f: File) => f.type === "video");
+      const imageFiles = files.filter((f: File) => f.type === "image");
+
+      if (videoFile && imageFiles.length) {
+        this.errorMessage = "only 1 video per upload allowed";
+        return null;
+      } else if (!videoFile && imageFiles.length > 4) {
+        this.errorMessage = "only up to 4 images per upload allowed";
+        return null;
+      }
+
+      this.errorMessage = null;
+    }
   }
-
-  addImages() {
-    this.imageInput.click();
-  }
-
-  imageSelected() {
-    const { files } = this.imageInput;
-    if (files === null) return; 
-
-    const images = [...files]; // FileList -> Array
-
-    if (files.length > 4) {
-      console.log("Too many files");
-    }
-
-    const mimeTypes = this.getMimeTypes(images);
-
-    // Check for mimetypes that are not image or video
-    const invalidMimeTypeExists = this.containsInvalidMimetypes(mimeTypes);
-    if (invalidMimeTypeExists) {
-      console.log("User needs to upload only images or video");
-    }
-
-    const validTypes = this.uniqueTypes(images);
-    
-    if (validTypes.size > 1) {
-      console.log("Image and video uploaded");
-    }
-
-    console.log("やった");
-    }
-
-    getMimeTypes(images: File[]) {
-      return images.map(image => image.type);
-    }
-
-    containsInvalidMimetypes(mimeTypes: string[]) {
-      const notValidMimeType = 
-        (mimeType: string) => !this.validMediaTypes.test(mimeType);
-      
-      const invalidMimeType = mimeTypes.find(notValidMimeType);
-      
-      return !!invalidMimeType;
-    }
-
-    uniqueTypes(images: File[]) {
-      const validMediaTypes = /(video|image)\//;
-      const fileTypes = images.map(file => file.type.match(validMediaTypes)![0]);
-      const uniqueTypes = new Set(fileTypes); 
-      return uniqueTypes;
-    }
-}
 </script>
